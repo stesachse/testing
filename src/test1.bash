a0b6387ba89e02c00d3195da1b9a563757dee1e0
Stephan Sachse <ste.sachse@gmail.com>
2018-09-28 17:17:39 +0200

    add footer and header

--- src/test1.bash
+++ src/test1.bash
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+# FILE: src/test1.bash
+
 printf 'running bash version: %s\n' "${BASH_VERSION}"
 printf 'got %d argument(s)\n' "${#}"
 
 exit 0
 
+# END OF FILE
+#
+# vim: expandtab tabstop=4 shiftwidth=4 softtabstop=4:
